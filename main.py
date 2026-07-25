9c499598c051ce9f5a816a2f680ee1aa626dabd6
mr-a998 <ateeqmalikz01@gmail.com>
2026-07-25 02:33:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ class ARScannerApp(ctk.CTk):
     def __init__(self):
         super().__init__()
 
-        self.title("AR Academy Scanner")
+        self.title("AR_Cyber_Academy Scanner")
         self.geometry("1280x820")
         self.minsize(1100, 700)
         self.configure(fg_color=CLR_BG)
